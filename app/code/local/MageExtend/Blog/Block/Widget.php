77786aacd0aeeaa6db890edc8c34890755382da5
dnyomo <dnyomo@briteskies.com>
2012-08-10 15:28:35 -0400

    test

--- app/code/local/MageExtend/Blog/Block/Widget.php
+++ app/code/local/MageExtend/Blog/Block/Widget.php
@@ -5,7 +5,6 @@ class MageExtend_Blog_Block_Widget extends Mage_Core_Block_Abstract {
         parent::_construct();
             if (Mage::getStoreConfig('blog_options/jquery/include_jquery_frontend')) {
                 Mage::app()->getLayout()->getBlock('head')->addJs('mageextend/blog/jquery-1.7.1.min.noConflict.js');
-                echo 'test';
             }
         }
     }
